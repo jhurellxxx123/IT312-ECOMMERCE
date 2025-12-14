6464a6c2cc5a0d8cb75cd28da8969901fa589e88
Your Name <you@example.com>
2025-12-14 17:40:08 +0800

    deploying issue status 1

--- backend/server.js
+++ backend/server.js
@@ -31,11 +31,12 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get(".*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+    // CHANGE THIS LINE: Remove quotes " " and use slashes / /
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
 }
 
 app.listen(PORT, () => {
